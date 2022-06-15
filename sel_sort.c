27662841c7d3347ee7183c7be6a7b29afbc7d822
Umgoel <76695806+Umgoel@users.noreply.github.com>
2022-06-15 09:56:09 +0530

    Add files via upload

--- sel_sort.c
+++ sel_sort.c
@@ -1,41 +1,86 @@
 #include<stdio.h>
-#include<time.h>
 #include<stdlib.h>
+#include<time.h>
 
-int n, arr1[10000];
-void swap(int *a, int *b){
-int temp = *a;
-*a = *b;
-*b = temp;
+void selsort(int n, int a[]);
+
+void main()
+{
+    int a[15000],n,i,j,ch,temp;
+    clock_t start,end;
+    while(1)
+{
+    printf("\n 1:For manual entry of N value and array elements ");
+    printf("\n 2:To display time taken for sorting number of elements N in the range 500 to 14500");
+    printf("\n 3:To exit ");
+    printf("\n Enter your choice:");
+    scanf("%d", &ch);
+
+
+switch(ch)
+{
+    case 1: printf("\nEnter the number of elements: ");
+    scanf("%d",&n);
+    printf("\nEnter array elements: ");
+    for(i=0;i<n;i++)
+    {
+    scanf("%d",&a[i]);
+    }
+    start=clock();
+    selsort(n,a);
+    end=clock();
+
+    printf("\nSorted array is: ");
+    for(i=0;i<n;i++)
+    printf("%d\t",a[i]);
+
+    printf("\n Time taken to sort %d numbers is %f Secs",n, (((double)(end-start))/CLOCKS_PER_SEC));
+    break;
+
+    case 2:
+        n=500;
+        while(n<=14500)
+         {
+            for(i=0;i<n;i++)
+{
+a[i]=n-i;
 }
-void init_array(int size){
-int k;
-for(k=0;k<size;k++){
-arr1[k] = k*2;
+start=clock();
+selsort(n,a);
+
+//Dummy loop to create delay
+for(j=0;j<500000;j++)
+    { temp=38/600;}
+end=clock();
+
+printf("\n Time taken to sort %d numbers is %f Secs ",n, (((double)(end-start))/CLOCKS_PER_SEC));
+n=n+1000;
+}
+break;
+
+case 3: exit(0);
+}
+getchar();
 }
 }
-void sel_sort(int size){
-int min_index,i,j;
-for(i=0;i<size-1;i++){
-    min_index = i;
-    for(j=i+1;j<size;j++){
-        if(arr1[j] > arr1[min_index])
-            min_index = j;
-        }
-swap(&arr1[min_index],&arr1[i]);
+
+void selsort(int n,int a[])
+{
+int i,j,t,small,pos;
+for(i=0;i< n-1;i++)
+{
+pos=i;
+small=a[i];
+for(j=i+1;j<n;j++)
+{
+if(a[j]<small)
+{
+small=a[j];
+pos=j;
 }
 }
-void print(int size){
-int i;
-for(i=0;i<size;i++){
-    printf("  %d  ",arr1[i]);
+t=a[i];
+a[i]=a[pos];
+a[pos]=t;
 }
-printf("\n");
-}
-void main(){
-    int size = 5;
-    init_array(size);
-    print(size);
-    sel_sort(size);
-    print(size);
 }
